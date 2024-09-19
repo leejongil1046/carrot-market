a0b42810120b5081b42c3a4742f52fcb2b8dbc81
leejongil1046 <leejongil1046@gmail.com>
2024-09-19 19:06:34 +0900

    Tailwind CSS - Profile

--- pages/chats/[id].tsx
+++ pages/chats/[id].tsx
@@ -2,7 +2,7 @@ import type { NextPage } from "next";
 
 const ChatDetail: NextPage = () => {
   return (
-    <div className="py-10 px-4 space-y-4">
+    <div className="py-10 pb-16 px-4 space-y-4">
       <div className="flex items-start space-x-2">
         <div className="w-8 h-8 rounded-full bg-slate-400" />
         <div className="w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">
@@ -165,8 +165,8 @@ const ChatDetail: NextPage = () => {
           <p>미쳤어</p>
         </div>
       </div>
-      <div className="fixed w-full mx-auto max-w-md bottom-2 inset-x-0">
-        <div className="flex relative items-center">
+      <div className="fixed py-2 bg-white bottom-0 inset-x-0">
+        <div className="flex relative max-w-md items-center w-full mx-auto">
           <input
             type="text"
             className="shadow-sm rounded-full w-full border-gray-300 focus:ring-orange-500 focus:outline-none pr-12 focus:border-orange-500"
